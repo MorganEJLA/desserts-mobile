476dbd317af22ba81fc6e9e737e216a355139049
MorganEJLA <mojones311@gmail.com>
2025-03-04 19:50:59 -0500

    updated shape of data to rdy for API

--- src/features/restaurants/screens/restaurants.screen.js
+++ src/features/restaurants/screens/restaurants.screen.js
@@ -6,6 +6,7 @@ import { SafeArea } from '../../../components/utility/safe-area.component'
 import { RestaurantInfoCard } from '../components/restaurant-info-card.component'
 import { Spacer } from '../../../components/spacer/spacer.component'
 
+import { RestaurantsContext } from '../../../services/restaurants/restaurants.context'
 const SearchContainer = styled.View`
   padding: ${(props) => props.theme.space[3]};
 `
@@ -15,33 +16,21 @@ const RestaurantList = styled(FlatList).attrs({
   },
 })``
 export const RestaurantsScreen = () => {
+  const { isLoading, error, restaurants } = useContext(RestaurantsContext)
   return (
     <SafeArea>
       <SearchContainer>
         <Searchbar />
       </SearchContainer>
       <FlatList
-        data={[
-          { name: 1 },
-          { name: 2 },
-          { name: 3 },
-          { name: 4 },
-          { name: 5 },
-          { name: 6 },
-          { name: 7 },
-          { name: 8 },
-          { name: 9 },
-          { name: 10 },
-          { name: 11 },
-          { name: 12 },
-          { name: 13 },
-          { name: 14 },
-        ]}
-        renderItem={() => (
-          <Spacer position="bottom" size="large">
-            <RestaurantInfoCard />
-          </Spacer>
-        )}
+        data={restaurants}
+        renderItem={({ item }) => {
+          return (
+            <Spacer position="bottom" size="large">
+              <RestaurantInfoCard restaurant={item} />
+            </Spacer>
+          )
+        }}
         keyExtractor={(item) => item.name}
       />
     </SafeArea>
